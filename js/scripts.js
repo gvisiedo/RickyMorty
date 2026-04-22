c93e4e8b5edbffe379125536e4c2c68061828c8b
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-04-22 07:39:39 +0200

    Character Searching, and resolving errors

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+const rickyInput = document.querySelector('#personaje')
+const btnBuscar = document.querySelector('#btn-buscar')
+const resultado = document.querySelector('#resultado')
+
+btnBuscar.addEventListener('click', async function buscarPersonaje() {
+  if(rickyInput.value){  // validar antes de pedir
+  
+  try {
+    resultado.textContent = 'Buscando...'
+    const respuesta = await fetch(`https://rickandmortyapi.com/api/character/?name=${rickyInput.value}`)
+    const datos = await respuesta.json()
+    
+    if(datos.error){ // gestionar error de la API
+        resultado.textContent = 'Este personaje no es de Ricky Morty'
+        return
+    }else{
+       const nombre = datos.results[0].name     // nombre del personaje
+       const status = datos.results[0].status   // "Alive", "Dead" o "unknown"
+       const species = datos.results[0].species  // especie
+       const origin = datos.results[0].origin.name  // planeta de origen
+
+       resultado.textContent = `Nombre: ${nombre}, Status: ${status}, Species: ${species}, Origin: ${origin} ` 
+    }  
+    
+
+  } catch(error) {
+    resultado.textContent = 'Algo no ha ido bien' + error}
+        }else{
+        resultado.textContent = 'Indica un personaje '// gestionar error de red
+    }
+})
